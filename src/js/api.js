581307794dbae819a7d5aa49fae7c8975885c6e2
Oleksii Levchenko <neonator3000@gmail.com>
2025-08-28 17:39:33 +0300

    fix api
    fix artists

--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import Pagination from 'tui-pagination';
 
 
+export let page = 1
 
 export async function fetchArtists() {
   try {
-    const res = await axios.get("https://sound-wave.b.goit.study/api/artists", { params: { limit: 8 } });
-    
+    const res = await axios.get("https://sound-wave.b.goit.study/api/artists", { params: { limit: 8, page } });
+    page += 1;
     return res.data.artists;
   } catch (err) {
     console.error("Помилка:", err);
--- src/js/artists.js
+++ src/js/artists.js
@@ -1,8 +1,12 @@
 import { fetchArtists } from "./api";
+import { page } from "./api";
 
-function createListArtists (images){
-      const markup = images.map(({ strArtistThumb, genres, strArtist, strBiographyEN }) => `
-  <div class="card__media">
+const button = document.querySelector('.load-more')
+
+function createListArtists(images) {
+  const markup = images.map(({ strArtistThumb, genres, strArtist, strBiographyEN }) => `
+  <div class="card">
+      <div class="card__media">
     <img src="${strArtistThumb}" alt="${strArtist}" />
   </div>
   <div class="card__body">
@@ -15,11 +19,43 @@ function createListArtists (images){
       <a class="card__link" href="#">Learn More →</a>
     </div>
   </div>
+  </div>
 `).join('');
-
-document.getElementById('gallery').innerHTML = markup;
+  const gallery = document.querySelector('.gallery')
+  gallery.insertAdjacentHTML('beforeend', markup)
+  page += 1
 }
 
 fetchArtists().then(artists => {
   createListArtists(artists);
 });
+
+function loadMore(images) {
+
+  const markup = images.map(({ strArtistThumb, genres, strArtist, strBiographyEN }) => `
+  <div class="card">
+      <div class="card__media">
+    <img src="${strArtistThumb}" alt="${strArtist}" />
+  </div>
+  <div class="card__body">
+    <div class="tags">
+      ${genres.map(genre => `<span class="tag">${genre}</span>`).join('')}
+    </div>
+    <h3 class="card__title">${strArtist}</h3>
+    <p class="card__text">${strBiographyEN}</p>
+    <div class="card__footer">
+      <a class="card__link" href="#">Learn More →</a>
+    </div>
+  </div>
+  </div>
+`).join('');
+
+  const gallery = document.querySelector('.gallery')
+  gallery.insertAdjacentHTML('beforeend', markup)
+}
+
+button.addEventListener('click', () => {
+  fetchArtists().then(artists => {
+    loadMore(artists)
+  })
+})
