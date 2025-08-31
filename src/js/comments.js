0ada55a9eba94c94e273603f4bc8df76185c2fb2
Nazar <popadyc123nazar@gmail.com>
2025-08-31 13:58:21 +0100

    almost finished section

---
+++ src/js/comments.js
@@ -0,0 +1,176 @@
+import 'css-star-rating/css/star-rating.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+const overlay = document.querySelector(".overlay");
+const openBtn = document.querySelector(".feedback-btn")
+const closeBtn = document.querySelector(".close-icon");
+const form = document.querySelector("#feedback-form");
+const container = document.querySelector(".feedback-section");
+
+
+// modal
+openBtn.addEventListener("click", (e) => {
+    overlay.classList.remove("hidden");
+  container.classList.add("hidden");
+  document.body.classList.add("no-scroll");
+})
+
+closeBtn.addEventListener("click", (e) => {
+    overlay.classList.add("hidden");
+  container.classList.remove("hidden");
+  document.body.classList.remove("no-scroll");
+});
+
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    overlay.classList.add("hidden")
+    container.classList.remove("hidden");
+    document.body.classList.remove("no-scroll");
+  }
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    overlay.classList.add("hidden")
+    container.classList.remove("hidden")
+    document.body.classList.remove("no-scroll");
+  }
+})
+
+// stars
+function createStars(container, rating) {
+  container.innerHTML = "";
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement("span");
+    star.classList.add("star");
+    if (i <= rating) star.classList.add("filled");
+    container.appendChild(star);
+    if (container.id === "customer-rating") {
+      star.addEventListener("click", (e) => {
+        container.dataset.rating = i;
+        createStars(container, i);
+      })
+    }
+  }
+}
+
+// data from API
+const formRating = document.getElementById("customer-rating");
+createStars(formRating, parseInt(formRating.dataset.rating) || 0);
+
+const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+  pagination: { el: '.swiper-pagination', clickable: true },
+        grabCursor: true,
+});
+
+async function loadReviews() {
+  const wrapper = document.querySelector(".swiper-wrapper");
+  try {
+    const data = await fetch("https://sound-wave.b.goit.study/api/feedbacks");
+    const json = await data.json();
+    const feedbacks = json.data.slice(0, 10);
+    feedbacks.forEach((feedback, index) => {
+      const stars = Math.round(feedback.rating);
+
+      const slide = document.createElement("div");
+      slide.classList.add("swiper-slide")
+
+      slide.innerHTML = `<div class="rating" id="rating-${index}" data-rating="${stars}"></div>
+                <div class="feedback">
+                    <p class="customer-feedback">${feedback.descr}</p>
+                    <h3 class="customer-name">${feedback.name}</h3>
+                </div>`;
+      wrapper.appendChild(slide);
+      const starContainer = slide.querySelector(".rating");
+      createStars(starContainer, stars);
+    });
+    swiper.update()
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+// pagination
+const firstDot = document.getElementById('first');
+const secondDot = document.getElementById('second');
+const thirdDot = document.getElementById('third');
+function activeDot(index, totalSlides) {
+  [firstDot, secondDot, thirdDot].forEach(dot => dot.classList.remove('active'));
+  if (index === 0) {
+    firstDot.classList.add('active')
+  } else if (index === totalSlides - 1) {
+    thirdDot.classList.add('active')
+  } else {
+    secondDot.classList.add('active')
+  }
+}
+swiper.on('slideChange', () => {
+  activeDot(swiper.activeIndex, swiper.slides.length);
+});
+activeDot(swiper.activeIndex, swiper.slides.length);
+
+firstDot.addEventListener('click', () => swiper.slideTo(0));
+secondDot.addEventListener('click', () => swiper.slideTo(1));
+thirdDot.addEventListener('click', () => swiper.slideTo(swiper.slides.length - 1));
+
+// data to API
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const name = form.querySelector(".form-input-name").value.trim();
+  const message = form.querySelector(".form-input-message").value.trim()
+  const rating = Math.round(formRating.dataset.rating) || 0;
+  console.log("Name:", name);
+  console.log("Message:", message);
+  console.log("Rating:", rating);
+  function showError(msg) {
+    alert(msg);
+  }
+    if (!name || name.length < 2 || name.length > 16) {
+    return showError("Shortest name - 2 letters; Largest name - 16 letters")
+  };
+  if (!message || message.length < 10 || message.length > 512) {
+    return showError("Min message - 10 symbols; Max message - 512 symbols")
+  }
+  if (rating < 1 || rating > 5) {
+    return showError("Rating must be between '1' and '5'")
+  }
+  try {
+    const response = await fetch("https://sound-wave.b.goit.study/api/feedbacks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        descr: message,
+        rating: rating,
+      })
+      
+    })
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    } 
+    console.log("Feedback is processed");
+
+    form.reset();
+    formRating.dataset.rating = 0;
+    createStars(formRating, 0);
+    overlay.classList.add("hidden");
+    container.classList.remove("hidden");
+    document.body.classList.remove("no-scroll");
+    loadReviews();
+  } catch (err) {
+    console.error(err);
+    alert("Bad request (invalid request body)")
+  }
+})
+document.addEventListener("DOMContentLoaded", loadReviews);
